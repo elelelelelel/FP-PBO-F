4d93d595c9fa3056de7cd9cd5cc40b437a00097e
agnesfiaa <94664966+agnesfiaa@users.noreply.github.com>
2021-12-20 16:28:30 +0700

    Add files via upload

---
+++ tugasgame/src/MouseInput.java
@@ -0,0 +1,53 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+
+public class MouseInput implements MouseListener, MouseMotionListener {
+	
+	public int x,y;
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+				
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+				
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+				
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		for(int i=0;i<GamePlay.launcher.buttons.length;i++) {
+			Button button = GamePlay.launcher.buttons[i];
+			
+			if(x>=button.getX()&&y>=button.getY()&&x<=button.getX()+button.getWidth()&&y<=button.getY()+button.getHeight()) 
+				button.triggerEvent();
+		}
+				
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+				
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+				
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+				
+	}
+
+}
